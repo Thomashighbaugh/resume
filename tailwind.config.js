8ed773012757bccf64fa20f0dec5e0920d0367a9
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-03-14 11:14:46 -0700

    fixing the print mode still

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,7 @@ module.exports = {
                 letter: "72rem",
             },
             height: {
-                letter: "85.9375rem",
+                letter: "83.9375rem",
                 "letter-col": "71.625rem",
                 "letter-col-full": "77.9375rem",
             },
